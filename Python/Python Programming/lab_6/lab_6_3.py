d284a7391fe445a363377c4c85a4117659c8d170
张佳伟 <15961023232@163.com>
2026-05-22 11:12:03 +0800

    lab_6_3

--- Python/Python Programming/lab_6/lab_6_3.py
+++ Python/Python Programming/lab_6/lab_6_3.py
@@ -1,2 +1,8 @@
 def monkey_peach(n):
-    
+    if n == 10:
+        return 1
+    dangqian_peach = 2 * (monkey_peach(n + 1) + 1)
+    return dangqian_peach
+
+first_day_total = monkey_peach(1)
+print(first_day_total)
